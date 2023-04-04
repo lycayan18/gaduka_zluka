2787db7c2d9f6467e82195be23caebb592702842
DungyBug <ruslanbez2006@gmail.com>
2023-04-04 14:05:46 +0300

    add authors and minor changes

--- src/components/main-page/index.tsx
+++ src/components/main-page/index.tsx
@@ -6,7 +6,7 @@ export default function MainPage() {
         <div className="main-page-content">
             <h1>Приветствуем на GadukaZluka.io!</h1>
             <p>
-                GadukaZluka.io - это чат-приложение, где люди общаются в чатах в реальном времени.
+                <b>GadukaZluka.io</b> - это чат-приложение, где люди общаются в чатах в реальном времени.
             </p>
             <p>
                 Чаты разделены на ветки, каждая из которых добавляет новую особенность чату.
@@ -21,6 +21,49 @@ export default function MainPage() {
             </ul>
             <br/>
             <h4>Выбирайте себе любой чат и общайтесь!</h4>
+
+            <div className="authors">
+                <span>Авторы проекта:</span>
+                <div className="author">
+                    <div className="author-nickname">DungyBug</div>
+                    <ul className="account-links">
+                        <li>
+                            <img src="https://telegram.org/img/website_icon.svg" width="20" alt="" />
+                            <a href="https://t.me/dungybug" target="_blank">@dungybug</a>
+                        </li>
+                        <li>
+                            <img src="https://github.githubassets.com/favicons/favicon.svg" width="20" alt="" />
+                            <a href="https://github.com/DungyBug" target="_blank">DungyBug</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="author">
+                    <div className="author-nickname">drakutont</div>
+                    <ul className="account-links">
+                        <li>
+                            <img src="https://telegram.org/img/website_icon.svg" width="20" alt="" />
+                            <a href="https://t.me/drakutont" target="_blank">@drakutont</a>
+                        </li>
+                        <li>
+                            <img src="https://github.githubassets.com/favicons/favicon.svg" width="20" alt="" />
+                            <a href="https://github.com/DRAKUTONT" target="_blank">DRAKUTONT</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="author">
+                    <div className="author-nickname">Kirill</div>
+                    <ul className="account-links">
+                        <li>
+                            <img src="https://telegram.org/img/website_icon.svg" width="20" alt="" />
+                            <a href="https://t.me/zaboluev" target="_blank">@zaboluev</a>
+                        </li>
+                        <li>
+                            <img src="https://github.githubassets.com/favicons/favicon.svg" width="20" alt="" />
+                            <a href="https://github.com/ZaboluevK" target="_blank">ZaboluevK</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
