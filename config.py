f483b1acff8d9c547c0fa2105a4b67e690522274
DungyBug <ruslanbez2006@gmail.com>
2023-04-15 16:10:04 +0300

    remove garbage code

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very_secret_string'
 app.config['SQLALCHEMY_DATABASE_URI'] = environ['DB_PATH']
 
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app)
 
 database = SQLAlchemy(app)
